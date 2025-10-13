64be5c3552839ee41c93d4795a26aa787a213724
derhore <philippe196568@gmail.com>
2025-10-13 17:07:30 +0000

    Ajout du fichier wsgi.py pour le déploiement

--- monquestionnaire/wsgi.py
+++ monquestionnaire/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for monquestionnaire project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+# monquestionnaire/wsgi.py
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monquestionnaire.settings")
+# Définir la variable d'environnement pour les settings Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monquestionnaire.settings')
 
+# Charger l'application WSGI Django
 application = get_wsgi_application()
+
+# Configurer WhiteNoise pour servir les fichiers statiques
+# Le chemin '/app/staticfiles' est utilisé par Railway pour les fichiers statiques
+application = WhiteNoise(application, root='/app/staticfiles', prefix='static/')

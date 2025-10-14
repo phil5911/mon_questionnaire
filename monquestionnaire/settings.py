b03e69f80ff28a0b63f549742617af78908c7143
derhore <philippe196568@gmail.com>
2025-10-14 10:20:52 +0000

    Enable debug and allow all hosts

--- monquestionnaire/settings.py
+++ monquestionnaire/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 # --- ALLOWED_HOSTS ---
 if DEBUG:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+    ALLOWED_HOSTS = ['*']
 else:
     ALLOWED_HOSTS = ['monquestionnaire-production.up.railway.app']
 
